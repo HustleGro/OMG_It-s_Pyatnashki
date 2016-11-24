89581f50cbe7c887501771ca9efb8d94a90efcec
HustleGro <rrommb@gmail.com>
2016-11-24 21:01:50 +0200

    Add files via upload

--- OMG_It`s_Pyatnashki/src/Ratings.java
+++ OMG_It`s_Pyatnashki/src/Ratings.java
@@ -9,24 +9,25 @@ import java.awt.SystemColor;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import java.awt.Color;
-public class Ratings /*extends JFrame*/ {
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Ratings extends JFrame {
   
-  private JFrame contentPane;
-  
-  public static void main(String[] args) {
-    EventQueue.invokeLater(new Runnable() {
-      public void run() {
-        try {
-          Ratings window = new Ratings();
-          window.contentPane.setVisible(true);
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-      }
-    });
-  }
-  
-  public Ratings() {  
+  JFrame contentPane;//**************
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ratings frame = new Ratings();
+					frame.contentPane.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+ public Ratings() {  
     initialize();
   }
   
@@ -34,33 +35,30 @@ public class Ratings /*extends JFrame*/ {
     contentPane = new JFrame();
 //  contentPane.getContentPane().setForeground(SystemColor.windowBorder);
     contentPane.setTitle("Ratings");
-    contentPane.setBounds(100, 100, 500, 500); //(х,у,ширина, высота)
+    contentPane.setBounds(100, 100, 500, 500); //(С…,Сѓ,С€РёСЂРёРЅР°, РІС‹СЃРѕС‚Р°)
     contentPane.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     contentPane.getContentPane().setLayout(null);
     
-    JButton btnNewButton = new JButton("Вернуться в главное меню");
-    btnNewButton.setBounds(85, 415, 300, 54);
+    JButton btnNewButton = new JButton("Р’РµСЂРЅСѓС‚СЊСЃСЏ РІ РіР»Р°РІРЅРѕРµ РјРµРЅСЋ");
+    btnNewButton.addActionListener(new ActionListener() {
+    	public void actionPerformed(ActionEvent e) {
+    		contentPane.setVisible(false);
+    		InterFaZe intr = new InterFaZe();
+    		intr.frmPyatnashki.setVisible(true);
+    	}
+    });
+    btnNewButton.setBounds(100, 396, 300, 54);
     btnNewButton.setBackground(SystemColor.activeCaption);
     btnNewButton.setForeground(Color.DARK_GRAY);
-    btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));//шрифт
+    btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));//С€СЂРёС„С‚
     contentPane.getContentPane().add(btnNewButton);
     
-    JLabel lblNewLabel = new JLabel("Рейтинг");
+    JLabel lblNewLabel = new JLabel("Р РµР№С‚РёРЅРі");
     lblNewLabel.setBackground(SystemColor.menu);
     lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
     lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
     lblNewLabel.setBounds(170, 20, 132, 47);
     contentPane.getContentPane().add(lblNewLabel); 
-    /*
-     setTitle("Ratings");
-     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-     setBounds(100, 100, 450, 300);
-     contentPane = new JPanel();
-     contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-     contentPane.setLayout(new BorderLayout(0, 0));
-     setContentPane(contentPane);
-     
-     JPanel panel = new JPanel();
-     contentPane.add(panel, BorderLayout.NORTH);*/
-  }
+	}
+
 }
